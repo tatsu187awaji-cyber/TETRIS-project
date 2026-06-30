383037515721098edbed58d57f2127f1efeb7c86
tatsuki yamasaki <tatsuki.yamasaki@g.dreamcareer.co.jp>
2026-06-30 11:10:11 +0900

    jsにプラグイン導入

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 export default defineConfig({
   base: "/TETRIS-project/",
+  plugins: [createHtmlPlugin()],
 });
